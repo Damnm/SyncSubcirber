f7de217449cd4665ff723dbe1c940aad1f19b7b5
thinhnth <thinhnth@epayjsc.com>
2023-11-02 09:02:56 +0700

    commit AdminDbContext

---
+++ src/EPAY.ETC.Core.Sync-Subcriber.Core/Models/Entities/AdminBaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAY.ETC.Core.Sync_Subcriber.Core.Models.Entities
+{
+    public class AdminBaseEntity<TId>
+    {
+        public TId? Id { get; set; }
+
+        [MaxLength(255)]
+        public string? Name { get; set; }
+        [StringLength(2)]
+        public string? StationId { set; get; }
+    }
+}
---
+++ src/EPAY.ETC.Core.Sync-Subcriber.Core/Models/Enums/ReconciliationActionEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace EPAY.ETC.Core.Sync_Subcriber.Core.Models.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum ReconciliationActionEnum
+    {
+        [EnumMember(Value = "Tất cả")]
+        All,
+        [EnumMember(Value = "Hợp lệ")]
+        Valid,
+        [EnumMember(Value = "Không hợp lệ")]
+        Invalid,
+        [EnumMember(Value = "Xem xét")]
+        Consider
+    }
+}
