87bcae6878b9fb0b7176bb9bb07ae6bd7161f6b4
longnguyen1331 <longnguyen13th1d.pou@gmail.com>
2023-11-07 11:04:32 +0700

    add test folder

---
+++ src/UnitTests/EPAY.ETC.Core.Sync-Subcriber.Core.UnitTests/Extensions/EnumHelperExtensionTests.cs
@@ -0,0 +1,11 @@
+namespace EPAY.ETC.Core.Sync_Subcriber.Core.UnitTests.Extensions
+{
+    public class EnumHelperExtensionTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/UnitTests/TestProject1/Services/SyncServiceTests.cs
@@ -0,0 +1,11 @@
+namespace EPAY.ETC.Core.Sync_Subcriber.Infrastructure.UnitTests.Services
+{
+    public class SyncServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
