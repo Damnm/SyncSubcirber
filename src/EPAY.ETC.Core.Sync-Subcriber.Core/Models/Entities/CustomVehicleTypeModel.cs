1b20cbe3047cb50de7ae291b163a0502912737f6
longnguyen1331 <longnguyen13th1d.pou@gmail.com>
2023-11-07 10:07:49 +0700

    Long : updatecode call api

---
+++ src/EPAY.ETC.Core.Sync-Subcriber.Core/Models/Entities/CustomVehicleTypeModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EPAY.ETC.Core.Sync_Subcriber.Core.Models.Entities
+{
+    [Table("CustomVehicleType")]
+    public class CustomVehicleTypeModel : BaseEntity<Guid>
+    {
+        [MaxLength(6)]
+        public string ExternalId { get; set; }
+    }
+}
--- src/EPAY.ETC.Core.Sync-Subcriber.Infrastructure/Services/SyncServices.cs
+++ src/EPAY.ETC.Core.Sync-Subcriber.Infrastructure/Services/SyncServices.cs
@@ -3,8 +3,6 @@ using EPAY.ETC.Core.Sync_Subcriber.Core.Models.LaneTransaction;
 using EPAY.ETC.Core.Sync_Subcriber.Infrastructure.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Npgsql.PostgresTypes;
-using System.Globalization;
 
 namespace EPAY.ETC.Core.Sync_Subcriber.Infrastructure.Services
 {
@@ -42,7 +40,7 @@ namespace EPAY.ETC.Core.Sync_Subcriber.Infrastructure.Services
                    VehicleDetails = new VehicleLaneOutDetailRequestModel
                    {
                        RFID = p.Payment.RFID,
-                       VehicleTypeId = p.Payment.CustomVehicleTypeId.ToString(),
+                       VehicleTypeId =  p.Payment.CustomVehicleType.ExternalId,
                        FrontPlateColour = p.Payment.Fee.PlateColour,
                        FrontPlateNumber = p.Payment.Fee.PlateNumber,
                        FrontImage = p.Payment.Fee.LaneOutVehiclePhotoUrl,
