012cbc868a1a26558d59f264f633f7b4cc424bed
Mạnh Quốc <manhquoc11@gmail.com>
2023-11-28 08:23:56 +0700

    Update docker build shell script

--- build-main.sh
+++ build-main.sh
@@ -10,12 +10,11 @@ tag=$image:$timestamp
 latest=$image:latest  
   
 #build image  
-docker build --no-cache -t $tag -f Dockerfile . 
+docker build --no-cache -t $latest -t $tag -f Dockerfile . 
 
 #push to dockerhub  
 docker login  
 #sudo docker login -u username -p password  
-docker image tag $tag $latest
 
 docker push $tag
 docker push $latest
